a22aacb715b6d37e9a0da60c1767f6a5d86c7503
Balaji A <abj.balaji@gmail.com>
2022-04-02 19:16:33 +0530

    conditions

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -e components/$1.sh ]; then
-  echo "component does not exist"
-fi
+ if [ -e components/$1.sh ]; then
+   echo "component does not exist"
+ fi
+
 bash components/$1.sh
