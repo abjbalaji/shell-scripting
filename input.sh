85de8666578b87b6015c5d13cb8674da89bb34dc
Balaji A <abj.balaji@gmail.com>
2022-04-02 16:49:16 +0530

    input

--- input.sh
+++ input.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 read -p 'Enter your name :' name
 echo your name is : $name
+
+echo script name  : $0
+echo 1st argument is :$1
+echo 2nd arguments is : $2
+echo all the args : $*
+echo no. of args :$#
