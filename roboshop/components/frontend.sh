b512a0f234a5e2f432c4c95668466dd40af29bee
Balaji A <abj.balaji@gmail.com>
2022-04-03 11:27:35 +0530

    roboshop

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,18 +25,20 @@ print "Downloading Nginx package"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 StatCheck $?
 
-print "Cleaning old Nginx files and uzipping new files"
-StatCheck $?
+print "Cleaning old Nginx files "
 rm -rf /usr/share/nginx/html/*
-cd /usr/share/nginx/html/
-unzip /tmp/frontend.zip
+StatCheck $?
 
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
+cd /usr/share/nginx/html/
+
+print "Extracting Zip file and Moving the files"
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+StatCheck $?
+
+print "Updating roboshop configuration"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+StatCheck $?
 
 print "Starting Nginx"
-systemctl restart nginx
-systemctl enable nginx
+systemctl restart nginx && systemctl enable nginx
 StatCheck $?
