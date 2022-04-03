f8b2251edb3d0e948e6f9490c9d8af377ea0a365
Balaji A <abj.balaji@gmail.com>
2022-04-03 11:09:50 +0530

    function

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+StatCheck(){
+  if [ $1 -eq 0 ]; then
+              echo -e "\e[34mSUCCESS\e[0m"
+            else
+              echo -e "\e[31mFAILURE\e[0m"
+              exit 1
+            fi
+            }
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0]
 then
@@ -8,32 +16,18 @@ then
 fi
 echo -e "\e[32mInstalling Nginx\e[0m"
 yum install nginx -y
-if [ $? -eq 0 ]; then
-  echo -e "\e[34mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatCheck $?
 
 echo -e "\e[33mDownloading Nginx package \e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "\e[34mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatCheck $?
 
 echo -e "\e[31mCleaning old Nginx files and uzipping new files\e[0m"
+StatCheck $?
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
-if [ $? -eq 0 ]; then
-  echo -e "\e[34mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
@@ -42,9 +36,4 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[32mStarting Nginx\e[0m"
 systemctl restart nginx
 systemctl enable nginx
-if [ $? -eq 0 ]; then
-echo -e "\e[34mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+StatCheck $?
