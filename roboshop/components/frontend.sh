5df3627c42561dcd04f82ad2f5c33b70ada61637
Balaji A <abj.balaji@gmail.com>
2022-04-05 21:44:45 +0530

    Mongodb

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-StatCheck(){
-  if [ $1 -eq 0 ]; then
-              echo -e "\e[34mSUCCESS\e[0m"
-            else
-              echo -e "\e[31mFAILURE\e[0m"
-              exit 1
-            fi
-            }
-print(){
-  echo -e "\n............$1..........." &>>$LOG_FILE
-  echo -e "\e[32m $1 \e[0m"
-}
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ]
-then
-  echo you should run with root user
-  exit 1
-fi
+source components/common.sh
 print "Installing Nginx"
 yum install nginx -y &>>$LOG_FILE
 StatCheck $?
