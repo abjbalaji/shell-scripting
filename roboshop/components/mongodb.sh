030861848a2e75b4023ec78e2b073722baff281c
Balaji A <abj.balaji@gmail.com>
2022-04-05 21:35:39 +0530

    Mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -23,12 +23,15 @@ fi
 print " Setup YUM repos "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
 StatCheck $?
+
 print " Installing Mongodb "
 yum install -y mongodb-org &>>LOG_FILE
 StatCheck $?
+
 print " Updating Mongodb address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 StatCheck $?
+
 print " Starting Mongodb"
 systemctl enable mongod &>>LOG_FILE && systemctl start mongod &>>LOG_FILE
 StatCheck $?
